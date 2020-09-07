4d38bb4b18945283de2cd6a83f3b09669177bf68
AndreiSuprun <66207171+AndreiSuprun@users.noreply.github.com>
2020-09-07 19:44:13 +0300

    Update ApplianceDAOImpl.java

--- main/java/com/suprun/criteria/dao/impl/ApplianceDAOImpl.java
+++ main/java/com/suprun/criteria/dao/impl/ApplianceDAOImpl.java
@@ -4,32 +4,72 @@ import com.suprun.criteria.dao.ApplianceDAO;
 import com.suprun.criteria.dao.DAOException;
 import com.suprun.criteria.dao.creator.CreatorCommand;
 import com.suprun.criteria.dao.creator.CreatorProvider;
-import com.suprun.criteria.dao.function.ParameterPredicate;
-import com.suprun.criteria.dao.source.ApplianceSource;
 import com.suprun.criteria.entity.Appliance;
 import com.suprun.criteria.entity.criteria.Criteria;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.ResourceBundle;
+import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ApplianceDAOImpl implements ApplianceDAO {
 
+    private static final String RESOURCE_FILE = "properties.file";
+    private static final String FILE_NAME = "filename";
+    private static final String PARAMETER_SELECTOR = ".*%s=(?i)(%s)[,\\s+]*.*";
+
     @Override
     public List<Appliance> find(Criteria criteria) throws DAOException {
-        Path applianceSourcePath = new ApplianceSource().receiveApplianceSourcePath();
-        ParameterPredicate predicate = new ParameterPredicate();
-        CreatorProvider creatorProvider = new CreatorProvider();
+        Path applianceSourcePath = receiveApplianceSourcePath();
+        BiPredicate<String, Criteria> predicate = getParameterPredicate();
+        CreatorProvider creatorProvider = CreatorProvider.getInstance();
         CreatorCommand creatorCommand = creatorProvider.receiveCommand(criteria.getGroupSearchName());
         List<Appliance> appliances;
-        try (Stream<String> applianceStream = Files.lines(applianceSourcePath)) {
+        try {
+            Stream<String> applianceStream = Files.lines(applianceSourcePath);
             appliances = applianceStream.filter(line -> predicate.test(line, criteria)).map(creatorCommand::create).collect(Collectors.toList());
         } catch (IOException e) {
             throw new DAOException("File doesn't available", e);
         }
         return appliances;
+    }
+
+    private Path receiveApplianceSourcePath() throws DAOException {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle(RESOURCE_FILE);
+        String fileName = resourceBundle.getString(FILE_NAME);
+        Path path;
+        try {
+            path = Path.of(fileName);
+        } catch (InvalidPathException e) {
+            throw new DAOException("Invalid file path", e);
+        }
+        return path;
+    }
+
+    private BiPredicate<String, Criteria> getParameterPredicate(){
+        BiPredicate<String, Criteria> predicate = new BiPredicate<String, Criteria>() {
+            @Override
+            public boolean test(String line, Criteria criteria) {
+                if (!line.contains(criteria.getGroupSearchName())){
+                    return false;
+                }
+                boolean isParameterValid = false;
+                for (String criteriaSearch : criteria.getSearchCriteriaSet()) {
+                    String valueSelector = String.format(PARAMETER_SELECTOR, criteriaSearch, criteria.getSearchCriteriaValue(criteriaSearch));
+                    if (line.contains(criteriaSearch) && line.matches(valueSelector)) {
+                        isParameterValid = true;
+                    } else {
+                        return false;
+                    }
+                }
+                return isParameterValid;
+            }
+        };
+        return predicate;
     }
 }
